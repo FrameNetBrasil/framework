d8a7fc80e28fadba00573cf7b45baeb286fae255
Ely Edison Matos <ely.matos@gmail.com>
2025-01-29 15:04:38 -0300

    Update link-button.blade.php

--- src/UI/components/link-button.blade.php
+++ src/UI/components/link-button.blade.php
@@ -2,9 +2,9 @@
 <button
     class = "ui medium {{$color}} button"
 >
-    @if($icon != '')
-        <i class="icon material">{{$icon}}</i>
-    @endif
+{{--    @if($icon != '')--}}
+{{--        <i class="icon material">{{$icon}}</i>--}}
+{{--    @endif--}}
     {{$label}}
     {{$slot}}
 </button>
