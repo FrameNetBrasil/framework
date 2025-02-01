a0738116f817e09be380d25ff5b4b7e2ceeb56a4
Ely Edison Matos <ely.matos@gmail.com>
2025-01-31 22:45:24 -0300

    Update concept.blade.php

--- src/UI/components/element/concept.blade.php
+++ src/UI/components/element/concept.blade.php
@@ -1,4 +1,4 @@
 <div class="flex align-items-center">
     <div><x-fw::icon.concept></x-fw::icon.concept></div>
-    <div  class="color_semantictype">{{$name}}</div>
+    <div  class="color_concept">{{$name}}</div>
 </div>
